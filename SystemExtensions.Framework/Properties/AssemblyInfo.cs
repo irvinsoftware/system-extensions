62c30d5fa990fbcec24372121865c55ce5f2b4c2
Nate Irvin <nate.irvin@gmail.com>
2020-02-08 22:23:12 -0700

    revising compile targets

--- SystemExtensions.Framework/Properties/AssemblyInfo.cs
+++ SystemExtensions.Framework/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SystemExtensions")]
+[assembly: AssemblyTitle("SystemExtensions.Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Irvin Software Products")]
-[assembly: AssemblyCopyright("Copyright © Irvin Software Products 2013-2014")]
+[assembly: AssemblyCompany("Irvin Software Products, LLC")]
+[assembly: AssemblyCopyright("Copyright © Irvin Software Products 2013-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("eb57847b-5692-4cad-9de4-d29b1cfa1dfb")]
 
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 
