566ed428e57057ee9eacee044f72f87364033c3f
Nate Irvin <nate.irvin@gmail.com>
2022-05-09 12:53:04 -0600

    adding class

---
+++ SystemExtensions.Standard/UInt8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Irvin.Extensions
+{
+    public struct UInt8
+    {
+        private byte Value { get; set; }
+        
+        public static implicit operator UInt8(int source)
+        {
+            if (source > byte.MaxValue || source < 0)
+            {
+                throw new InvalidCastException();
+            }
+
+            return new UInt8 { Value = (byte)source };
+        }
+    }
+}
