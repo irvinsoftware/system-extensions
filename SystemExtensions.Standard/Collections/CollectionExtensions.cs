e41b70c978140f7dd884035b76e87ac11e314983
Nate Irvin <nate.irvin@gmail.com>
2023-04-11 22:40:08 -0600

    updates

--- SystemExtensions.Standard/Collections/CollectionExtensions.cs
+++ SystemExtensions.Standard/Collections/CollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace Irvin.Extensions.Collections
 {
@@ -7,6 +9,11 @@ namespace Irvin.Extensions.Collections
         public static int LastIndex(this ICollection list)
         {
             return list.Count - 1;
+        }
+
+        public static bool IsOneOf<T>(this T value, params T[] possibleValues)
+        {
+            return possibleValues.Any(x => x.Equals(value));
         }
     }
 }
